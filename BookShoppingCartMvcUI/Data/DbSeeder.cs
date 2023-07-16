5ffe1e62dbcc194c71fa97f3f5feb7f7ee20b309
saba satseradze <saba.satseradze0@gmail.com>
2023-07-16 07:00:57 -0700

    add: Roles

---
+++ BookShoppingCartMvcUI/Data/DbSeeder.cs
@@ -0,0 +1,33 @@
+﻿using BookShoppingCartMvcUI.Constants;
+using Microsoft.AspNetCore.Identity;
+
+namespace BookShoppingCartMvcUI.Data
+{
+    public class DbSeeder
+    {
+        public static async Task SeedDefaultData(IServiceProvider service)
+        {
+            var userManager = service.GetService<UserManager<IdentityUser>>();
+            var roleManager = service.GetService<RoleManager<IdentityRole>>();
+            // adding some roles to db
+            await roleManager.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Roles.User.ToString()));
+
+            // create admin user
+            var admin = new IdentityUser
+            {
+                UserName = "admin",
+                Email = "admin@example.com",
+                EmailConfirmed = true,
+            };
+
+            var userInDb = await userManager.FindByEmailAsync(admin.Email);
+            if (userInDb is null) 
+            {
+                await userManager.CreateAsync(admin, "Admin@123");
+                await userManager.AddToRoleAsync(admin, Roles.Admin.ToString());
+
+            }
+        }
+    }
+}
--- BookShoppingCartMvcUI/Models/OrderStatus.cs
+++ BookShoppingCartMvcUI/Models/OrderStatus.cs
@@ -7,6 +7,9 @@ namespace BookShoppingCartMvcUI.Models
     public class OrderStatus
     {
         public int Id { get; set; }
+        
+        [Required]
+        public int StatusId { get; set; }
 
         [Required, MaxLength(20)]
         public string? StatusName { get; set; }
